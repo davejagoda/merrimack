bde342e20486d3a0c90f0d527a1656d12fada3d7
Dave Jagoda <davejagoda@gmail.com>
2014-08-25 11:21:08 -0700

    add domains.txt and merrimack.sh

---
+++ merrimack.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# version 0.1
+# main shell script, suitable for calling from cron
+
+TopLevelLogDirectory="$HOME/Merrimack/log"
+if [ ! -d $TopLevelLogDirectory ]; then
+    echo 'no top level log directory, creating'
+    mkdir -p $TopLevelLogDirectory
+fi
+echo $TopLevelLogDirectory
+
+for domain in `grep -v ^\# domains.txt`
+do
+    echo $domain
+    DomainDirectory="$TopLevelLogDirectory/$domain"
+    if [ ! -d $DomainDirectory ]; then
+        echo 'no domain directory, creating'
+        mkdir -p $DomainDirectory
+    fi
+    echo 'http'
+    curl -s $domain >> "$DomainDirectory/http`date "+%Y-%m-%dT%H:%M:%S%z"`.log"
+    echo 'dns'
+    dig $domain ANY >> "$DomainDirectory/dns`date "+%Y-%m-%dT%H:%M:%S%z"`.log"
+done
