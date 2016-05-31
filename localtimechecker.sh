d9b01891fe9147228a41080f93076f2f61809c75
dj <davejagoda@users.noreply.github.com>
2016-05-31 13:46:46 -0700

    Update localtimechecker.sh to print on else clauses

--- localtimechecker.sh
+++ localtimechecker.sh
@@ -7,11 +7,15 @@ lt=/etc/localtime
 # is it a symlink?
 
 if [ -L $lt ]; then
-  echo $lt "is a symlink"
+    echo $lt "is a symlink"
+else
+    echo $lt "is not a symlink"
 fi
 
 # is it a valid file?
 
 if [ -L $lt ]; then
-  echo $lt "is a valid file"
+    echo $lt "is a valid file"
+else
+    echo $lt "is not a valid file"
 fi
